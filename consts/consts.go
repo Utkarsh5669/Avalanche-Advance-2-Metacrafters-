3e79fde92486c1c217a7f9f31aba2440a16816c8
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2024-08-27 01:02:56 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = "ManishChain"
+	HRP = "UtkarshArora"
 	// TODO: choose a name for your hyperchain
-	Name = "Manish"
+	Name = "Utkarsh"
 	// TODO: choose a token symbol
-	Symbol = "MHN"
+	Symbol = "UA"
 )
 
 var ID ids.ID
